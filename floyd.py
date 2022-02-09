1c6054c3973a9f96d65bf45cc81e2eb88b19231a
arsenijj <arsenijjnickitin2001@gmail.com>
2022-02-09 20:28:56 +0400

    Solution for Floyd's algo

--- floyd.py
+++ floyd.py
@@ -1,26 +1,22 @@
-n = int(input())
-g = [[] for i in range(n)]
-used = [False] * n
+from math import inf
+
+
+def get_data():
+    n = int(input())
+    g = [[inf if weight == '0' else int(weight) for weight in input().split()] for _ in range(n)]
+    return n, g
+
+
+n, graph = get_data()
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            d = graph[i][k] + graph[k][j]
+            if d < graph[i][j]:
+                graph[i][j] = d
+
+graph = [[0 if path_length == inf else path_length for path_length in row] for row in graph]
 
 for i in range(n):
-    temp = input().split()
-    for j in range(n):
-        if int(temp[j]) == 1:
-            g[i].insert(0, j)
-ans = []
-
-
-def dfs(v):
-    used[v] = True
-    for i in range(len(g[v])):
-        to = g[v][i]
-        if not used[to]:
-            dfs(to)
-    ans.insert(0, v + 1)
-
-
-for i in range(n):
-    if not used[i]:
-        dfs(i)
-
-print(*ans)
+    print(*graph[i])
